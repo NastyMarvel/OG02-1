3ca45fca8048abbe134629217b038b8cf04a2893
NastyMarvel <pachystachys@yandex.ru>
2024-12-30 21:38:41 +0300

    Update DZ.py
    это прошлое какое то дз

--- DZ.py
+++ DZ.py
@@ -1,1 +1,18 @@
-print("hello word")
+import random
+
+classroom_size = 30
+
+def generate_random_students():
+    names = ['Алексей', 'Виктор', 'Дмитрий', 'Иван', 'Мария']
+    students = []
+    for name in names:
+        students.append(name)
+    while len(students) < classroom_size:
+        students.extend(names)
+    return students
+
+def choose_random_student():
+    return random.choice(generate_random_students())
+
+for _ in range(5):
+    print(choose_random_student())
